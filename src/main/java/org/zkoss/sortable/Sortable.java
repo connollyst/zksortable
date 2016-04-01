4a1518e60e6b4f60baaeff75b27b4bac4987b4a6
Sean Connolly <connolly.st@gmail.com>
2016-04-01 15:44:24 +1000

    Added support for 'group' and 'animation' settings.

--- src/main/java/org/zkoss/sortable/Sortable.java
+++ src/main/java/org/zkoss/sortable/Sortable.java
@@ -1,9 +1,37 @@
 package org.zkoss.sortable;
 
+import org.zkoss.zk.ui.sys.ContentRenderer;
 import org.zkoss.zul.impl.XulElement;
+
+import java.io.IOException;
 
 /**
  * @author Sean Connolly
  */
 public class Sortable extends XulElement {
+
+    private String group;
+    private Integer animation;
+
+    public void setGroup(String group) {
+        if (this.group == null || group == null || !this.group.equals(group)) {
+            this.group = group;
+            smartUpdate("group", group);
+        }
+    }
+
+    public void setAnimation(Integer animation) {
+        if (this.animation == null || animation == null || !this.animation.equals(animation)) {
+            this.animation = animation;
+            smartUpdate("animation", animation);
+        }
+    }
+
+    @Override
+    protected void renderProperties(ContentRenderer renderer) throws IOException {
+        super.renderProperties(renderer);
+        render(renderer, "group", group);
+        render(renderer, "animation", animation);
+    }
+
 }
--- src/main/resources/web/js/zul/sortable/Sortable.js
+++ src/main/resources/web/js/zul/sortable/Sortable.js
@@ -1,9 +1,21 @@
 zul.sortable.Sortable = zk.$extends(zk.Widget, {
+    $define: {
+        sortable: function (s) {
+            var n = this.$n();
+            if (n)
+                s.sortable = s;
+        },
+        group: null,
+        animation: null
+    },
     bind_: function () {
         this.$supers(zul.sortable.Sortable, 'bind_', arguments);
         var config = {
-            // TODO settings
+            group: this._group,
+            animation: this._animation
         };
-        $('#' + this.uuid).sortable(config);
+        this.setSortable(
+            $('#' + this.uuid).sortable(config)
+        );
     }
 });
