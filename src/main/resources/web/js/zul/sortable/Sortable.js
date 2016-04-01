9b3223b02ea4b60d767b9bb314a8d00704bdf072
Sean Connolly <connolly.st@gmail.com>
2016-04-01 15:33:54 +1000

    sortable divs

--- src/main/resources/web/js/zul/sortable/Sortable.js
+++ src/main/resources/web/js/zul/sortable/Sortable.js
@@ -1,1 +1,9 @@
-zul.sortable.Sortable = zk.$extends(zk.Widget, {});
+zul.sortable.Sortable = zk.$extends(zk.Widget, {
+    bind_: function () {
+        this.$supers(zul.sortable.Sortable, 'bind_', arguments);
+        var config = {
+            // TODO settings
+        };
+        $('#' + this.uuid).sortable(config);
+    }
+});
